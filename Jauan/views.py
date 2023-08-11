fc9dad7252d7ef9cf6273771795fcba15c9e8a22
Victor <victorribeiro172@gmail.com>
2023-08-11 17:18:15 -0300

    coloração da linguagem

--- Jauan/views.py
+++ Jauan/views.py
@@ -15,6 +15,7 @@ from argparse import ArgumentParser
 def index(request):
     context = {}
     dados = ""
+    dados = chamaJasmin()
     context["saidaLog"] = dados
     template = loader.get_template("Jauan/index.html")
     return HttpResponse(template.render(context, request))
@@ -22,46 +23,29 @@ def index(request):
 
 @csrf_exempt
 def minha_view(request):
-    # context = {}
-    # if request.method == 'POST':
-    #     uploaded_file = request.POST['file']
-    #     print('ff\n\n', uploaded_file)
-    #     file = uploaded_file.read().decode('utf-8')
-    #     with open("input.txt", "w") as f:
-    #         f.write(file)
-    #     context['conteudo'] = file
     return 0
 
 @csrf_exempt
 def save_text_file(request):
     if request.method == "POST":
         content = request.POST.get("content")
-        with open("input.jauan", "w") as f:
+        with open("input.txt", "w") as f:
             f.write(content)
-            chamaJasmin()
-        return HttpResponse(content)
+        dados = chamaJasmin()
+        return HttpResponse(dados)
     return HttpResponse("Erro: método não permitido.")
 
 def chamaJasmin():
     args = ArgumentParser()
-    args.add_argument('-f', '--file', help='File to compile', default='input.jauan')
-    args = args.parse_args(['-f', 'input.jauan'])  # Fornecendo argumentos simulados
+    args.add_argument('-f', '--file', help='File to compile', default='input.txt')
+    args = args.parse_args(['-f', 'input.txt'])
     data = FileStream(args.file, encoding='utf8')
-
-    # data = InputStream(exp)
-
-    # Lexer
     lexer = jauanLexer(data)
     tokens = CommonTokenStream(lexer)
-
-    # Parser
     parser = jauanParser(tokens)
     tree = parser.prog()
-
-    # Listener
     walker = ParseTreeWalker()
     l = MyListener()
     walker.walk(l, tree)
-
     compile('programaJasmin')
     execute('programaJasmin')
--- programaJasmin.j
+++ programaJasmin.j
@@ -10,35 +10,36 @@ invokespecial java/util/Scanner/<init>(Ljava/io/InputStream;)V
 astore 0
 ldc 0
 istore 1
+L3:
+iload 1
+ldc 5
+if_icmpgt L4
+goto L5
+L4:
+ldc 1
+istore 4
+goto L6
+L5:
 ldc 0
-istore 2
-ldc 10
-istore 2
-iload 2
-ldc 10
-if_icmpeq L1
+istore 4
+L6:
+iload 4
+ifne L1
 goto L2
 L1:
-ldc 1
-istore 6
-goto L3
-L2:
-ldc 0
-istore 6
-L3:
-iload 6
-istore 1
 new java/lang/StringBuilder
 dup
 invokespecial java/lang/StringBuilder/<init>()V
-ldc "true"
-invokevirtual java/lang/StringBuilder/append(Ljava/lang/String;)Ljava/lang/StringBuilder;
+ldc 3
+invokevirtual java/lang/StringBuilder/append(I)Ljava/lang/StringBuilder;
 ldc " "
 invokevirtual java/lang/StringBuilder/append(Ljava/lang/String;)Ljava/lang/StringBuilder;
 invokevirtual java/lang/StringBuilder/toString()Ljava/lang/String;
-astore 9
+astore 6
 getstatic java/lang/System/out Ljava/io/PrintStream;
-aload 9
+aload 6
 invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V
+goto L3
+L2:
 return
 .end method
