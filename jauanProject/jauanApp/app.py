3d83a383cc8ec9335dd769e9d790572e1140a7be
MarceloJanio <marcelojanio@ufpi.edu.br>
2023-08-16 09:41:47 -0300

    teste botao

--- jauanProject/jauanApp/app.py
+++ jauanProject/jauanApp/app.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+from antlr4 import *
+
+from gen.jauanLexer import jauanLexer
+from gen.jauanParser import jauanParser
+from gen.MyListener import MyListener
+from jauanProject.jasmin import execute,compile
+
+
+from argparse import ArgumentParser
+
+
+
 import argparse
 from flask import Flask, render_template
 from flask_socketio import SocketIO
@@ -49,6 +61,30 @@ def read_and_forward_pty_output():
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/salvacodigo")
+def salvaCode():
+    args = ArgumentParser()
+    args.add_argument('-f', '--file', help='File to compile',default='input.jauan')
+    args = args.parse_args()
+    data = FileStream(args.file, encoding='utf8')
+    # data = InputStream(exp)
+
+    # Lexer
+    lexer = jauanLexer(data)
+    tokens = CommonTokenStream(lexer)
+
+    # Parser
+    parser = jauanParser(tokens)
+    tree = parser.prog()
+
+    # Listener
+    walker = ParseTreeWalker()
+    l = MyListener()
+    walker.walk(l, tree)
+
+    compile('programaJasmin')
+    print("salvando codigo")
 
 @socketio.on("pty-input", namespace="/pty")
 def pty_input(data):
